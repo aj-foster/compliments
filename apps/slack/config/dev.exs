a3501931380a72d6717eb8fe4bf6bc8ce907c3f1
AJ Foster <web@aj-foster.com>
2018-09-07 12:03:30 -0600

    Add configuration for Slack and Plug

---
+++ apps/slack/config/dev.exs
@@ -0,0 +1,3 @@
+use Mix.Config
+
+config :slack, shared_secret: "8f742231b10e8888abcd99yyyzzz85a5"
---
+++ apps/slack/config/test.exs
@@ -0,0 +1,4 @@
+use Mix.Config
+
+config :slack, shared_secret: "8f742231b10e8888abcd99yyyzzz85a5"
+config :plug, validate_header_keys_during_test: false
