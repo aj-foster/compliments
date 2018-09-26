53d7461b61391d1590f758359920b03a6c5a800a
AJ Foster <web@aj-foster.com>
2018-09-26 01:52:43 -0400

    Add help/error messages via Slack's response URL

--- apps/manager/lib/manager.ex
+++ apps/manager/lib/manager.ex
@@ -1,18 +1,70 @@
 defmodule Manager do
   @moduledoc """
-  Documentation for Manager.
+  Validates and responds to compliment commands.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Manager.hello()
-      :world
-
+  Accepts compliment command parameters, validates, and reacts.
   """
-  def hello do
-    :world
+  def compliment(params) do
+    with {:ok, params} <- parse_params(params),
+         {:ok, %{}} <- parse_text(params) do
+      :ok
+    else
+      {:error, :invalid_params} -> :error
+      {:ok, :help} -> :ok
+      {:error, :invalid_text} -> :error
+    end
+  end
+
+  # Extract necessary information from Slack's request.
+  defp parse_params(params) do
+    with {:ok, "/compliment"} <- Map.fetch(params, "command"),
+         {:ok, response_url} <- Map.fetch(params, "response_url"),
+         {:ok, text} <- Map.fetch(params, "text"),
+         {:ok, user_id} <- Map.fetch(params, "user_id") do
+      {:ok, %{response_url: response_url, text: text, user_id: user_id}}
+    else
+      :error -> {:error, :invalid_params}
+    end
+  end
+
+  # Extract necessary information from the /compliment [text].
+  defp parse_text(params) do
+    text =
+      params
+      |> Map.fetch!(:text)
+
+    cond do
+      String.match?(text, ~r/^\s*help/) ->
+        respond_with_help(params)
+        {:ok, :help}
+
+      true ->
+        respond_with_error(params)
+        {:error, :invalid_text}
+    end
+  end
+
+  defp respond_with_help(%{response_url: url}) do
+    help = """
+    {
+      "response_type": "ephemeral",
+      "text": "Example: `/compliment @JaneDoe Your work on our latest project was impressive...`"
+    }
+    """
+
+    HTTPoison.post(url, help, [{"Content-Type", "application/json"}])
+  end
+
+  defp respond_with_error(%{response_url: url}) do
+    help = """
+    {
+      "response_type": "ephemeral",
+      "text": "Example: `/compliment @JaneDoe Your work on our latest project was impressive...`"
+    }
+    """
+
+    HTTPoison.post(url, help, [{"Content-Type", "application/json"}])
   end
 end
--- apps/manager/test/manager_test.exs
+++ apps/manager/test/manager_test.exs
@@ -1,8 +1,41 @@
 defmodule ManagerTest do
   use ExUnit.Case
-  doctest Manager
 
-  test "greets the world" do
-    assert Manager.hello() == :world
+  import Mock
+
+  @help_request %{
+    "command" => "/compliment",
+    "response_url" =>
+      "https://hooks.slack.com/commands/T01234567/012345678901/AbcDEfGHiJklmNOpqeSTUVWX",
+    "team_id" => "T01234567",
+    "text" => "help",
+    "user_id" => "U01234567"
+  }
+
+  @error_request %{
+    "command" => "/compliment",
+    "response_url" =>
+      "https://hooks.slack.com/commands/T01234567/012345678901/AbcDEfGHiJklmNOpqeSTUVWX",
+    "team_id" => "T01234567",
+    "text" => "error",
+    "user_id" => "U01234567"
+  }
+
+  describe "compliment/1" do
+    test "sends help message" do
+      with_mock HTTPoison, post: fn _url, _data, _headers -> {:ok, %HTTPoison.Response{}} end do
+        Manager.compliment(@help_request)
+
+        assert called(HTTPoison.post(:_, :_, :_))
+      end
+    end
+
+    test "sends error message" do
+      with_mock HTTPoison, post: fn _url, _data, _headers -> {:ok, %HTTPoison.Response{}} end do
+        Manager.compliment(@error_request)
+
+        assert called(HTTPoison.post(:_, :_, :_))
+      end
+    end
   end
 end
